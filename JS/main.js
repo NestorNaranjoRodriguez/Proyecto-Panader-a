6a3d77a288beff3e919fd402aa73a34b9eed150c
NestorNaranjoRodriguez <nestornaranjorodriguez@alumno.ieselrincon.es>
2025-05-18 16:09:24 +0100

    Mejora de prodcutos 2

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,2 @@
 let carusel = document.getElementById("carusel")
 let buyButton = document.getElementById("buy_button")
-
-function showBuy(){
-    carusel.style.opacity = ".3"
-    buyButton.style.display = "block"
-}
-
-function hideBuy(){
-    carusel.style.opacity = "1"
-    buyButton.style.display = "none"
-}
